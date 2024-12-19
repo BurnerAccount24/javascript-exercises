fb2f8bfe34352f7d8db5838eaff5abfeb2f4cc1b
Mariam <meem7255@gmail.com>
2024-12-19 16:44:14 +0300

    finished some of the js exercises and did some experimentations in the code to solidify some of the concepts and the syntax.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let modifiedString = str.toLowerCase().replace(/[\W_]/g, '');
+	let reversedString = modifiedString.split('').reverse().join('');
+	return modifiedString === reversedString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+	num = Number(num);
+	if (num === 0) {
+		return 0;
+	} else if (num < 0) {
+		return 'OOPS';
+	}
+	let num1 = 0;
+	let num2 = 1;
+	let sum;
+	for (let i = 2; i <= num; i++) {
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+	}
 
+	return num2;
 };
 
 // Do not edit below this line
